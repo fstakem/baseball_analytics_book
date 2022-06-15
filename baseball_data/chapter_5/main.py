3eff783f43a7714f1995291d44ee5d2423cba966
Fredrick Stakem <fstakem@gmail.com>
2022-06-14 21:24:36 -0400

    Progress with chapter 5

--- baseball_data/chapter_5/main.py
+++ baseball_data/chapter_5/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pandas as pd
+from pandas.core.series import Series
 
 file_path = Path(__file__).parent
 project_path = Path(__file__).parent.parent.parent.resolve()
@@ -13,6 +14,27 @@ def str_to_value(row: str) -> str:
         return '1'
 
     return '0'
+
+
+def first_occupied_after_play(row: Series) -> int:
+    if row['RUN1_DEST_ID'] == 1 | row['BAT_DEST_ID'] == 1:
+        return 1
+
+    return 0
+
+
+def second_occupied_after_play(row: Series) -> int:
+    if row['RUN1_DEST_ID'] == 2 | row['RUN2_DEST_ID'] == 2 | row['BAT_DEST_ID'] == 2:
+        return 1
+
+    return 0
+
+
+def third_occupied_after_play(row: Series) -> int:
+    if row['RUN1_DEST_ID'] == 3 | row['RUN2_DEST_ID'] == 3 | row['RUN3_DEST_ID'] == 3 | row['BAT_DEST_ID'] == 3:
+        return 1
+
+    return 0
 
 
 fields_df = pd.read_csv(fields_path)
@@ -33,13 +55,23 @@ all_data_df['RUNS_ROI'] = all_data_df['MAX_RUNS'] - all_data_df['RUNS']
 
 all_data_df['BASES'] = all_data_df['BASE1_RUN_ID'].apply(str_to_value).astype(str) + \
                        all_data_df['BASE2_RUN_ID'].apply(str_to_value).astype(str) + \
-                       all_data_df['BASE3_RUN_ID'].apply(str_to_value).astype(str) + \
-                       ' ' + \
-                       all_data_df['OUTS_CT'].astype(str)
+                       all_data_df['BASE3_RUN_ID'].apply(str_to_value).astype(str)
+all_data_df['STATE'] = all_data_df['BASES'] + all_data_df['OUTS_CT'].astype(str)
 
-# all_data_df['BASES'] = (all_data_df['BASE1_RUN_ID'])
-# np.where(all_data_df['BASE1_RUN_ID'].str.len() > 0, 1, 0)
+all_data_df['NRUNNER1'] = all_data_df.apply(first_occupied_after_play, axis=1)
+all_data_df['NRUNNER2'] = all_data_df.apply(second_occupied_after_play, axis=1)
+all_data_df['NRUNNER3'] = all_data_df.apply(third_occupied_after_play, axis=1)
+all_data_df['NOUTS'] = all_data_df['OUTS_CT'] + all_data_df['EVENT_OUTS_CT']
+all_data_df['NEW_BASES'] = all_data_df['NRUNNER1'].astype(str) + \
+                           all_data_df['NRUNNER2'].astype(str) + \
+                           all_data_df['NRUNNER3'].astype(str)
+all_data_df['NEW_STATE'] = all_data_df['NEW_BASES'] + all_data_df['NOUTS'].astype(str)
 
+filtered_df = all_data_df[(all_data_df['STATE'] == all_data_df['NEW_STATE']) | (all_data_df['RUNS_SCORED'] > 0)]
+filtered_df = filtered_df[filtered_df['OUTS_INNINGS'] == 3]
+x = filtered_df.groupby('STATE')['RUNS_ROI'].mean()
+x_df = x.to_frame()
+x_df.reset_index(level=0)
 
 import ipdb
 ipdb.set_trace()
